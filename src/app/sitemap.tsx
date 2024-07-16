674b270d80fc253e51075783af281b5ed887a83c
hamza chbit <hamzachbit@gmail.com>
2024-07-16 14:27:24 +0000

    first commit

---
+++ src/app/sitemap.tsx
@@ -0,0 +1,15 @@
+import { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://imagetopdf-converter.vercel.app/',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    
+    
+
+  ]
+}
